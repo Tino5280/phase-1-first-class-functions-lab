f73b3297cedb914e223565798de32c2bc9f7bcca
Tino <tino5280@gmail.com>
2023-02-24 22:59:27 -0700

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+    
+    
+let scuberDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function() {
+    return scuberDrivers.slice(0, -2)
+}
+
+const returnLastTwoDrivers = function() {
+    return scuberDrivers.slice(2, 4)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(mult) {
+    return function(num) {
+        return (num * mult)
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(scuberDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers()
+}
