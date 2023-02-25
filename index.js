ef9d39c935cb5d044afe39cfa94cefbca3c3c02d
Tino <tino5280@gmail.com>
2023-02-24 22:46:54 -0700

    commit tree 511e2890f1958cc14265f6678927fb1d0017757b

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+    
+    
+let scuberDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function() {
+    return scuberDrivers.slice(0, -2)
+}
+
+const returnLastTwoDrivers = function() {
+    return scuberDrivers.slice(2, 4)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(mult) {
+    return function(num) {
+        return (num * mult)
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
